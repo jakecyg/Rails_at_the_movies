564796cf237237e8441e47beeda05fee99c73d6d
jakecyg <43617272+jakecyg@users.noreply.github.com>
2020-10-01 17:43:30 -0500

    _added validation to Movie model and dependency from production company model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
+  validates :title, :year, :duration, :description, :average_vote, presence: true
+  validates :title, uniqueness: true
+  validates :year, :duration, numericality: { only_integer: true }
+  validates :average_vote, numericality: true
   belongs_to :production_company
 end
