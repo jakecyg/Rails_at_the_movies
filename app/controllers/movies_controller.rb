3fe8d4a302544d420c81c09678d686d4e5457c5e
jakecyg <43617272+jakecyg@users.noreply.github.com>
2020-10-02 19:42:49 -0500

    implemented movies and ProdComp views + Bulma styling

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,12 @@
 class MoviesController < ApplicationController
   def index
+    # @movies = Movie.all
+    # above gets the job done but introduces n+1 when sub selects of the movie objects are made; ex. select the production comp for each movie queried
+    # solution:
+    @movies = Movie.includes(:production_company).order('average_vote DESC')
   end
 
   def show
+    @movie = Movie.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'production_companies/index'
-  get 'production_companies/show'
-  get 'movies/index'
-  get 'movies/show'
+  resources :movies, only: %i[index show]
+  resources :production_companies, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
