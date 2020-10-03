fb37cd0d974c4fac9b1cc87200a9c8e1bea05762
jakecyg <43617272+jakecyg@users.noreply.github.com>
2020-10-02 19:04:44 -0500

    generated movies and productioncompany controller and index/show views for each

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+Movie.delete_all
+ProductionCompany.delete_all
+
+filename = Rails.root.join('db/top_movies.csv')
+
+puts "Loading source csv from: #{filename}"
+
+csv_data = File.read(filename)
+movies = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
+
+movies.each do |m|
+  production_company = ProductionCompany.find_or_create_by(name: m['production_company'])
+
+  if production_company&.valid?
+    movie = production_company.movies.create(
+      title: m['original_title'],
+      year: m['year'],
+      duration: m['duration'],
+      description: m['description'],
+      average_vote: m['avg_vote']
+    )
+  else
+    puts "Invalid production company: #{production_company} for the movie #{m['original_title']}."
+  end
+end
+puts "Created #{ProductionCompany.count} production companies"
+puts "Created #{Movie.count} movies"
